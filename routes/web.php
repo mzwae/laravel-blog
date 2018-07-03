465f698b0aab11d012d4d94a2e71e2b8822c5328
mzwae <mrmonjaie@gmail.com>
2018-07-03 21:05:50 +0100

    added post ctrl and api

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
+Route::resource('posts', 'PostsController');
 
 Route::get('/hello', function () {
     return 'Hello World!';
