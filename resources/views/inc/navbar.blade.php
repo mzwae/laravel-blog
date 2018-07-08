a1432c4aa52bd5a6e5daf3ffa5571a857d3a5e37
mzwae <mrmonjaie@gmail.com>
2018-07-08 10:23:15 +0100

    aded base url

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,15 +1,15 @@
 
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
     <h5 class="my-0 mr-md-auto font-weight-normal">
-      <a class="p-2 text-dark" href="http://127.0.0.1:8080/laravel-blog/public/">
+      <a class="p-2 text-dark" href="<?=url('/')?>">
         {{config('app.name')}}
       </a>
     </h5>
     <nav class="my-2 my-md-0 mr-md-3">
-      <a class="p-2 text-dark" href="http://127.0.0.1:8080/laravel-blog/public/">Home</a>
-      <a class="p-2 text-dark" href="http://127.0.0.1:8080/laravel-blog/public/about">About</a>
-      <a class="p-2 text-dark" href="http://127.0.0.1:8080/laravel-blog/public/services">Services</a>
-      <a class="p-2 text-dark" href="http://127.0.0.1:8080/laravel-blog/public/posts">Posts</a>
-      <a class="p-2 text-dark" href="http://127.0.0.1:8080/laravel-blog/public/posts/create">Create Post</a>
+      <a class="p-2 text-dark" href="<?=url('/')?>">Home</a>
+      <a class="p-2 text-dark" href="<?=url('/')?>/about">About</a>
+      <a class="p-2 text-dark" href="<?=url('/')?>/services">Services</a>
+      <a class="p-2 text-dark" href="<?=url('/')?>/posts">Posts</a>
+      <a class="p-2 text-dark" href="<?=url('/')?>/posts/create">Create Post</a>
     </nav>
   </div>
