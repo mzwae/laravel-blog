8f3d77663e69834f8015dc4ded5a7956392525f4
mzwae <mrmonjaie@gmail.com>
2018-07-08 11:10:56 +0100

    added edit and delete post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,4 +10,8 @@
   </div>
   <hr>
   <a href="<?=url('/')?>/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+  {!!Form::open(['action'=>['PostsController@destroy', $post->id], 'class'=>'float-right'])!!}
+    {{Form::hidden('_method', 'DELETE')}}
+    {{Form::submit('Delete', ['class'=>'btn btn-danger'])}}
+  {!!Form::close()!!}
 @endsection
